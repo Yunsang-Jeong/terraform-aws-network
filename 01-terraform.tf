a6c0ea88d49e9dd7495e6ecbebc1aeb0336127c1
Yunsang-Jeong <79682061+Yunsang-Jeong@users.noreply.github.com>
2023-03-11 20:06:36 +0900

    update versions

--- 01-terraform.tf
+++ 01-terraform.tf
@@ -1,12 +1,11 @@
 terraform {
   # Terraform version
-  required_version = ">= 0.14.0"
-  experiments      = [module_variable_optional_attrs]
+  required_version = ">= 1.3.0"
   required_providers {
     # AWS provider version
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.25.0"
+      version = ">= 4.0.0"
     }
   }
 }
